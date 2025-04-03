f67214e25f049db79da5e35e6151088614ef2955
Ayushirani03 <150169944+Ayushirani03@users.noreply.github.com>
2025-04-03 22:18:57 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      {/* Hero */}
+      <Hero/>
+    </>
+  )
+}
+export default App
---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+export default {
+  plugins: [
+    import('tailwindcss'),
+    import('autoprefixer'),
+  
+   
+],
+};
